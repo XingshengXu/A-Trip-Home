d71d8e828a376e62c43001cb602d4bb1de15d67b
Xingsheng Xu <xuxingsheng163@gmail.com>
2023-04-11 16:15:17 -0700

    Several new features have been added to the game.
    1. A new TextTarget class has been added that generates random words and checks if the player types them correctly.
    
    2. A new CORRECT_TYPE event has been added to the event queue to handle the event when the player has typed the correct word.
    
    3. A new type_check method has been added to the TextTarget class to handle the typing event and update the word displayed on the screen.
    
    4. The jump_state method of the Cat class has been updated to check for the CORRECT_TYPE event instead of the space bar.
    
    5. The main_loop method of the Game class has been updated to include the new TextTarget class and draw it on the screen.
    
    6. The WORDBANK constant has been added to the settings.py file to store a list of possible words that can be displayed.
    
    7. The TARGET_FONT constant has been added to the settings.py file to store the font used to render the words displayed on the screen.

--- ATripHome.py
+++ ATripHome.py
@@ -1,6 +1,6 @@
 from sys import exit
 import pygame as pg
-
+from random import randint, choice
 from settings import *
 
 
@@ -29,12 +29,12 @@ class Cat(pg.sprite.Sprite):
         self.jump_sound = pg.mixer.Sound('assets/sound effect/Meow.ogg')
 
     def jump_state(self):
-        keys = pg.key.get_pressed()
-        if keys[pg.K_SPACE] and self.rect.bottom >= GROUND_HEIGHT:
-            self.gravity = -20
-            self.rect.y += self.gravity
-            self.jump_sound.play()
-            return
+        for event in pg.event.get():
+            if event.type == CORRECT_TYPE and self.rect.bottom >= GROUND_HEIGHT:
+                self.gravity = -20
+                self.rect.y += self.gravity
+                self.jump_sound.play()
+                return
 
         if self.rect.bottom < GROUND_HEIGHT:
             self.gravity += 1
@@ -48,7 +48,6 @@ class Cat(pg.sprite.Sprite):
             self.cat_index += 0.2
             if self.cat_index >= len(self.cat_jump):
                 self.cat_index = 0
-            print(int(self.cat_index))
             self.image = self.cat_jump[int(self.cat_index)]
         else:
             self.image = self.cat_stand
@@ -58,12 +57,50 @@ class Cat(pg.sprite.Sprite):
         self.animation()
 
 
+class TextTarget(pg.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+
+        self.letter_count = 0
+        self.candidate = choice(WORDBANK)
+        self.image = TARGET_FONT.render(self.candidate, False, (0, 0, 0))
+        self.rect = self.image.get_rect(center=(WIDTH//2, GROUND_HEIGHT//3))
+
+        self.hit_sound = pg.mixer.Sound('assets/sound effect/hit.wav')
+
+    def type_check(self):
+        if self.candidate:
+            keys = pg.key.get_pressed()
+            letter = self.candidate[self.letter_count]
+            letter_code = ord(letter)
+            for key in range(len(keys)):
+                if keys[key]:
+                    if key == letter_code:
+                        self.candidate = self.candidate.replace(letter, '', 1)
+                        self.hit_sound.play()
+                        self.image = TARGET_FONT.render(
+                            self.candidate, False, (0, 0, 0))
+                    else:
+                        break
+        else:
+            pg.event.post(pg.event.Event(CORRECT_TYPE))
+            self.letter_count = 0
+            self.candidate = choice(WORDBANK)
+            self.image = TARGET_FONT.render(self.candidate, False, (0, 0, 0))
+            self.rect = self.image.get_rect(
+                center=(WIDTH//2, GROUND_HEIGHT//3))
+
+    def update(self):
+        self.type_check()
+
+
 class Game:
     def __init__(self):
         pg.init()
         self.clock = pg.time.Clock()
         self.screen = pg.display.set_mode((WIDTH, HEIGHT))
         pg.display.set_caption('A Trip Home')
+
         try:
             self.sky_background = pg.image.load(
                 'assets/background/sky.png').convert()
@@ -91,14 +128,23 @@ class Game:
             self.screen.blit(self.ground_background,
                              (0, GROUND_HEIGHT))
 
+            text_target.draw(self.screen)
             player.draw(self.screen)
+
+            text_target.update()
             player.update()
+
             pg.display.update()
 
 
 # Run Main Loop
 
 game = Game()
+
+text_target = pg.sprite.GroupSingle()
+text_target.add(TextTarget())
+
 player = pg.sprite.GroupSingle()
 player.add(Cat())
+
 game.main_loop()
--- settings.py
+++ settings.py
@@ -1,3 +1,6 @@
+import pygame as pg
+pg.init()
+
 """
 The settings module for the "A Trip Home" game. This module contains all the default settings for the gameplay. 
 These settings can be easily accessed and used throughout the game by importing the module into the main game file.
@@ -8,3 +11,15 @@ GROUND_WIDTH = 200  # Ground Width
 GROUND_HEIGHT = HEIGHT - GROUND_WIDTH  # Ground Height
 CAT_WIDTH, CAT_HEIGHT = 162, 141  # Cat Dimension
 FPS = 60  # Game FPS
+TARGET_FONT = pg.font.Font('assets/font/BubblegumSans.ttf', 150)
+
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLACK = (0, 0, 0)
+
+# Game Events
+CORRECT_TYPE = pg.USEREVENT + 1
+
+
+WORDBANK = ["apple", "banana", "cactus", "dolphin", "elephant", "fitness", "guitar", "happiness", "island", "jacket", "kitchen", "lovely", "money", "network", "ocean", "paradise", "question", "romantic", "sunset", "tourist", "universe", "vacation", "wonderful", "xylophone", "yellow", "zebra", "airport", "beautiful", "capital", "dance", "education", "freedom", "government", "hospital", "internet", "journey", "knowledge", "library", "medicine", "natural", "oasis", "peaceful", "quality", "romance", "success", "television", "ultimate", "victory", "wealth", "extraordinary", "friendship", "generous", "hospitality", "imagination", "jubilant", "kindness", "laughter", "miracle", "nature", "optimistic", "passionate", "quest", "romanticism", "satisfied", "triumph", "unbelievable", "vibrant", "wisdom", "xenial", "youthful", "zestful", "adventure", "beauty", "challenge", "dazzling", "excitement", "friendship", "generosity", "honesty", "innovation", "joyful", "kindhearted", "luxury", "music", "optimism", "passion", "quaint", "romance", "satisfaction", "travelling", "understanding", "vitality", "warmth", "xtraordinary", "youthfulness", "zeal", "affection", "blessed", "charming", "delight", "enthusiasm", "fascinating", "generous", "humility", "innovative", "jovial", "kindness", "love", "marvelous", "nourishing", "optimistic", "peace", "quality", "relaxation", "sensual", "thriving", "uplifting", "vibrance",
+            "wholesome", "xceptional", "youthful", "zest", "abundance", "blissful", "calmness", "delightful", "exciting", "fantastic", "graceful", "harmony", "inspiration", "jubilation", "kindness", "lovely", "majestic", "nurturing", "oceanic", "pleasure", "quaintness", "rejuvenating", "serenity", "tranquility", "unforgettable", "vibrant", "wondrous", "xeniality", "youthfulness", "zealot", "affectionate", "breathtaking", "charming", "dazzling", "energetic", "flourishing", "glorious", "heavenly", "impressive", "joviality", "knightly", "loveable", "magnificent", "nourished", "oasis", "pleasurable", "quintessential", "refreshing", "spectacular", "thriving", "unforgotten", "vitality", "wondrous", "xpert", "yearning", "zestful", "adoration", "brilliant", "captivating", "delighted", "elegance", "fascinating", "gallant", "heartwarming", "innovative", "jubilant", "knight", "lovely", "mesmerizing", "nourishing", "optimistic", "peaceable", "pleased", "quaint", "relaxing", "satisfied", "tranquil", "unforgettable", "vibrance", "wholesome", "xceptional", "yielding", "zealous", "affectionate", "breathtaking", "charming", "dazzling", "enthusiastic", "flourishing", "glowing", "heavenly", "impressive", "jovial", "kindred", "loving", "mesmerizing", "nourished", "optimism", "peaceful", "pleased", "quaintness", "refreshing", "sensational", "thriving", "unforgotten", "vibrant", "warmhearted", "xpertise", "youthful", "zestful"]
