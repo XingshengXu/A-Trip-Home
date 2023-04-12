997abcd37687e8b813103fd5fb3dec47e294b495
Xingsheng Xu <xuxingsheng163@gmail.com>
2023-04-12 14:45:22 -0700

    Updated Version 0.2 1. The Cat class has been updated to include more animations. Instead of just standing, the cat now walks and jumps. 2. The Cat class now loads the cat walking and jumping images from a list of files stored in the CAT_WALK and CAT_JUMP variables defined in the settings module. 3. The Cat class now has a jump_state method that updates the cat's position based on gravity when the player correctly types a word. 4. The Cat class now has an animation method that updates the cat's image based on whether the cat is jumping or walking. 5. The TextTarget class has been updated to include sound effects when the player correctly types a word. 6. The TextTarget class now selects a random word from the WORDBANK defined in the settings module to display as the typing target. 7. The Trees class has been updated to include different types of trees, loaded from a dictionary TREE_TYPE defined in the settings module. 8. The Trees class now animates and destroys trees as they move across the screen. 9. The Game class has been updated to include a timer that spawns trees at a specified frequency. 10. The Game class now displays the player's score on the screen. 11. The main_loop method has been updated to include updates for the text target and trees, in addition to the cat.

--- ATripHome.py
+++ ATripHome.py
@@ -9,28 +9,40 @@ class Cat(pg.sprite.Sprite):
         super().__init__()
         self.gravity = 0
         self.cat_index = 0
-        # Cat Stand Image
-        self.cat_stand = pg.image.load('assets/cat/Stand.png').convert_alpha()
-        self.cat_stand = pg.transform.scale(
-            self.cat_stand, (CAT_WIDTH, CAT_HEIGHT))
-        self.image = self.cat_stand
+
+        # Cat Walk Image
+        self.cat_walk = []
+        for i in range(len(CAT_WALK)):
+            try:
+                image_cache = pg.image.load(CAT_WALK[i]).convert_alpha()
+            except Exception as e:
+                print(f'Error loading cat walk image: {e}')
+            image_cache = pg.transform.scale(
+                image_cache, (CAT_WIDTH, CAT_HEIGHT))
+            self.cat_walk.append(image_cache)
+        self.image = self.cat_walk[self.cat_index]
         self.rect = self.image.get_rect(
             midbottom=(WIDTH // 2, GROUND_HEIGHT))
 
         # Cat Jump Image and Sound
         self.cat_jump = []
-        for i in range(1, 9):
-            image_cache = pg.image.load(
-                f'assets/cat/Jump{i}.png').convert_alpha()
+        for i in range(len(CAT_JUMP)):
+            try:
+                image_cache = pg.image.load(CAT_JUMP[i]).convert_alpha()
+            except Exception as e:
+                print(f'Error loading cat jump image: {e}')
             image_cache = pg.transform.scale(
                 image_cache, (CAT_WIDTH, CAT_HEIGHT))
             self.cat_jump.append(image_cache)
-
-        self.jump_sound = pg.mixer.Sound('assets/sound effect/Meow.ogg')
+        try:
+            self.jump_sound = pg.mixer.Sound(JUMP_SOUND)
+        except Exception as e:
+            print(f'Error loading jump sound: {e}')
 
     def jump_state(self):
         for event in pg.event.get():
-            if event.type == CORRECT_TYPE and self.rect.bottom >= GROUND_HEIGHT:
+            if event.type == CORRECT_TYPING and self.rect.bottom >= GROUND_HEIGHT:
+                self.cat_index = 0
                 self.gravity = -20
                 self.rect.y += self.gravity
                 self.jump_sound.play()
@@ -44,13 +56,15 @@ class Cat(pg.sprite.Sprite):
         self.rect.y += self.gravity
 
     def animation(self):
+        self.cat_index += 0.2
         if self.rect.bottom < GROUND_HEIGHT:
-            self.cat_index += 0.2
             if self.cat_index >= len(self.cat_jump):
                 self.cat_index = 0
             self.image = self.cat_jump[int(self.cat_index)]
         else:
-            self.image = self.cat_stand
+            if self.cat_index >= len(self.cat_walk):
+                self.cat_index = 0
+            self.image = self.cat_walk[int(self.cat_index)]
 
     def update(self):
         self.jump_state()
@@ -62,13 +76,17 @@ class TextTarget(pg.sprite.Sprite):
         super().__init__()
 
         self.letter_count = 0
+        self.score = 0
         self.candidate = choice(WORDBANK)
-        self.image = TARGET_FONT.render(self.candidate, False, (0, 0, 0))
+        self.image = TARGET_FONT.render(self.candidate, False, 'hotpink')
         self.rect = self.image.get_rect(center=(WIDTH//2, GROUND_HEIGHT//3))
 
-        self.hit_sound = pg.mixer.Sound('assets/sound effect/hit.wav')
+        try:
+            self.hit_sound = pg.mixer.Sound(HIT_SOUND)
+        except Exception as e:
+            print(f'Error loading hit sound: {e}')
 
-    def type_check(self):
+    def typing_check(self):
         if self.candidate:
             keys = pg.key.get_pressed()
             letter = self.candidate[self.letter_count]
@@ -79,41 +97,80 @@ class TextTarget(pg.sprite.Sprite):
                         self.candidate = self.candidate.replace(letter, '', 1)
                         self.hit_sound.play()
                         self.image = TARGET_FONT.render(
-                            self.candidate, False, (0, 0, 0))
+                            self.candidate, False, 'hotpink')
                     else:
                         break
         else:
-            pg.event.post(pg.event.Event(CORRECT_TYPE))
+            pg.event.post(pg.event.Event(CORRECT_TYPING))
             self.letter_count = 0
             self.candidate = choice(WORDBANK)
-            self.image = TARGET_FONT.render(self.candidate, False, (0, 0, 0))
+            self.image = TARGET_FONT.render(self.candidate, False, 'hotpink')
             self.rect = self.image.get_rect(
                 center=(WIDTH//2, GROUND_HEIGHT//3))
 
     def update(self):
-        self.type_check()
+        self.typing_check()
+
+
+class Trees(pg.sprite.Sprite):
+    def __init__(self, treeType):
+        super().__init__()
+
+        file_path = TREE_TYPE.get(treeType)
+        try:
+            tree_image = pg.image.load(file_path).convert_alpha()
+        except Exception as e:
+            print(f'Error loading tree image: {e}')
+        if treeType in ['grass_tree1', 'grass_tree2']:
+            self.image = tree_image
+        else:
+            self.image = pg.transform.scale(
+                tree_image, (TREE_WIDTH, TREE_HEIGHT))
+        self.rect = self.image.get_rect(midbottom=(
+            randint(WIDTH, WIDTH * 2), GROUND_HEIGHT))
+
+    def animation(self):
+        self.rect.x -= MOVING_SPEED
+
+    def destroy(self):
+        if self.rect.x <= -WIDTH:
+            self.kill()
+
+    def update(self):
+        self.animation()
+        self.destroy()
 
 
 class Game:
     def __init__(self):
         pg.init()
+        self.start_time = 0
+        self.score = 0
         self.clock = pg.time.Clock()
         self.screen = pg.display.set_mode((WIDTH, HEIGHT))
         pg.display.set_caption('A Trip Home')
 
+        # Timer
+        self.tree_timer = TREE_SPAWN
+        pg.time.set_timer(self.tree_timer, TREE_SPAWN_FREQ)
+
         try:
-            self.sky_background = pg.image.load(
-                'assets/background/sky.png').convert()
-            self.ground_background = pg.image.load(
-                'assets/background/ground.png').convert()
-        except pg.error:
-            print('Error loading background image!')
+            self.sky_background = pg.image.load(SKY_BACKGROUND).convert()
+            self.ground_background = pg.image.load(GROUND_BACKGROUND).convert()
+        except Exception as e:
+            print(f'Error loading background image: {e}')
 
         # Background Images Rescale
         self.sky_background = pg.transform.scale(
             self.sky_background, (WIDTH, HEIGHT))
         self.ground_background = pg.transform.scale(
             self.ground_background, (WIDTH, GROUND_WIDTH))
+
+    def display_score(self, score):
+        score_text = SCORE_FONT.render(
+            f'Score: {score}', False, 'white')
+        score_rect = score_text.get_rect(right=WIDTH)
+        self.screen.blit(score_text, score_rect)
 
     def main_loop(self):
         '''This is the game main loop.'''
@@ -124,14 +181,23 @@ class Game:
                     pg.quit()
                     exit()
 
+                if event.type == self.tree_timer:
+                    tree_type = choice(list(TREE_TYPE.keys()))
+                    trees.add(Trees(tree_type))
+
             self.screen.blit(self.sky_background, (0, 0))
-            self.screen.blit(self.ground_background,
-                             (0, GROUND_HEIGHT))
+            self.screen.blit(self.ground_background, (0, GROUND_HEIGHT))
+            self.display_score(self.score)
 
             text_target.draw(self.screen)
+            trees.draw(self.screen)
             player.draw(self.screen)
 
             text_target.update()
+            for event in pg.event.get():
+                if event.type == CORRECT_TYPING:
+                    self.score += 1
+            trees.update()
             player.update()
 
             pg.display.update()
@@ -147,4 +213,5 @@ text_target.add(TextTarget())
 player = pg.sprite.GroupSingle()
 player.add(Cat())
 
+trees = pg.sprite.Group()
 game.main_loop()
--- settings.py
+++ settings.py
@@ -10,16 +10,42 @@ WIDTH, HEIGHT = 1600, 900  # Game Window Display
 GROUND_WIDTH = 200  # Ground Width
 GROUND_HEIGHT = HEIGHT - GROUND_WIDTH  # Ground Height
 CAT_WIDTH, CAT_HEIGHT = 162, 141  # Cat Dimension
+TREE_WIDTH, TREE_HEIGHT = 264, 333
 FPS = 60  # Game FPS
 TARGET_FONT = pg.font.Font('assets/font/BubblegumSans.ttf', 150)
+SCORE_FONT = pg.font.Font('assets/font/Purrfect.ttf', 70)
 
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLACK = (0, 0, 0)
 
+TREE_SPAWN_FREQ = 3000
+MOVING_SPEED = 3
+
 # Game Events
-CORRECT_TYPE = pg.USEREVENT + 1
+CORRECT_TYPING = pg.USEREVENT + 1
+TREE_SPAWN = pg.USEREVENT + 2
 
+# Image Path
+SKY_BACKGROUND = 'assets/background/sky.png'
+GROUND_BACKGROUND = 'assets/background/ground.png'
+CAT_WALK = ['assets/cat/Walk1.png', 'assets/cat/Walk2.png', 'assets/cat/Walk3.png', 'assets/cat/Walk4.png', 'assets/cat/Walk5.png',
+            'assets/cat/Walk6.png', 'assets/cat/Walk7.png', 'assets/cat/Walk8.png', 'assets/cat/Walk9.png', 'assets/cat/Walk10.png']
 
+CAT_JUMP = ['assets/cat/Jump1.png', 'assets/cat/Jump2.png', 'assets/cat/Jump3.png', 'assets/cat/Jump4.png',
+            'assets/cat/Jump5.png', 'assets/cat/Jump6.png', 'assets/cat/Jump7.png', 'assets/cat/Jump8.png']
+TREE_TYPE = {
+    'common_tree': 'assets\plants\common_tree.png',
+    'cypress_tree1': 'assets\plants\cypress_tree1.png',
+    'cypress_tree2': 'assets\plants\cypress_tree2.png',
+    'grass_tree1': 'assets\plants\grass_tree1.png',
+    'grass_tree2': 'assets\plants\grass_tree2.png'
+}
+
+# Sound Path
+JUMP_SOUND = 'assets/sound effect/Meow.ogg'
+HIT_SOUND = 'assets/sound effect/hit.wav'
+
+# Word Bank
 WORDBANK = ["apple", "banana", "cactus", "dolphin", "elephant", "fitness", "guitar", "happiness", "island", "jacket", "kitchen", "lovely", "money", "network", "ocean", "paradise", "question", "romantic", "sunset", "tourist", "universe", "vacation", "wonderful", "xylophone", "yellow", "zebra", "airport", "beautiful", "capital", "dance", "education", "freedom", "government", "hospital", "internet", "journey", "knowledge", "library", "medicine", "natural", "oasis", "peaceful", "quality", "romance", "success", "television", "ultimate", "victory", "wealth", "extraordinary", "friendship", "generous", "hospitality", "imagination", "jubilant", "kindness", "laughter", "miracle", "nature", "optimistic", "passionate", "quest", "romanticism", "satisfied", "triumph", "unbelievable", "vibrant", "wisdom", "xenial", "youthful", "zestful", "adventure", "beauty", "challenge", "dazzling", "excitement", "friendship", "generosity", "honesty", "innovation", "joyful", "kindhearted", "luxury", "music", "optimism", "passion", "quaint", "romance", "satisfaction", "travelling", "understanding", "vitality", "warmth", "xtraordinary", "youthfulness", "zeal", "affection", "blessed", "charming", "delight", "enthusiasm", "fascinating", "generous", "humility", "innovative", "jovial", "kindness", "love", "marvelous", "nourishing", "optimistic", "peace", "quality", "relaxation", "sensual", "thriving", "uplifting", "vibrance",
             "wholesome", "xceptional", "youthful", "zest", "abundance", "blissful", "calmness", "delightful", "exciting", "fantastic", "graceful", "harmony", "inspiration", "jubilation", "kindness", "lovely", "majestic", "nurturing", "oceanic", "pleasure", "quaintness", "rejuvenating", "serenity", "tranquility", "unforgettable", "vibrant", "wondrous", "xeniality", "youthfulness", "zealot", "affectionate", "breathtaking", "charming", "dazzling", "energetic", "flourishing", "glorious", "heavenly", "impressive", "joviality", "knightly", "loveable", "magnificent", "nourished", "oasis", "pleasurable", "quintessential", "refreshing", "spectacular", "thriving", "unforgotten", "vitality", "wondrous", "xpert", "yearning", "zestful", "adoration", "brilliant", "captivating", "delighted", "elegance", "fascinating", "gallant", "heartwarming", "innovative", "jubilant", "knight", "lovely", "mesmerizing", "nourishing", "optimistic", "peaceable", "pleased", "quaint", "relaxing", "satisfied", "tranquil", "unforgettable", "vibrance", "wholesome", "xceptional", "yielding", "zealous", "affectionate", "breathtaking", "charming", "dazzling", "enthusiastic", "flourishing", "glowing", "heavenly", "impressive", "jovial", "kindred", "loving", "mesmerizing", "nourished", "optimism", "peaceful", "pleased", "quaintness", "refreshing", "sensational", "thriving", "unforgotten", "vibrant", "warmhearted", "xpertise", "youthful", "zestful"]
